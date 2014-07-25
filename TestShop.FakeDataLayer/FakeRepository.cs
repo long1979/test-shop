4fad5ac4dd2b8043dd059b46dfba72282d3b5336
Viktor Borisov <vik.borisov@outlook.com>
2014-07-25 18:49:38 +0700

    Добавил фейковую БД

---
+++ TestShop.FakeDataLayer/FakeRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TestShop.DataLayer.Interfaces;
+
+namespace TestShop.FakeDataLayer
+{
+	public class FakeRepository<T> : IRepository<T> where T : class, IEntityBase
+	{
+		private readonly IList<T> _entities;
+
+		public FakeRepository(IList<T> entities)
+		{
+			_entities = entities;
+		}
+
+		public IQueryable<T> GetAll()
+		{
+			return _entities.AsQueryable();
+		}
+
+		public T Get(int entityId)
+		{
+			return _entities.FirstOrDefault(e => e.EntityId == entityId);
+		}
+
+		public void Create(T entity)
+		{
+			_entities.Add(entity);
+		}
+
+		public void Update(T entity)
+		{
+			var obj = Get(entity.EntityId);
+
+
+			if (obj == null)
+				return;
+
+			var index = _entities.IndexOf(obj);
+
+			_entities.Remove(obj);
+			_entities.Insert(index, entity);
+		}
+
+		public void Delete(T entity)
+		{
+			var obj = Get(entity.EntityId);
+
+
+			if (obj == null)
+				return;
+
+			_entities.Remove(obj);
+		}
+	}
+}
---
+++ TestShop.FakeDataLayer/RepositoryFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TestShop.DataLayer.Interfaces;
+
+namespace TestShop.FakeDataLayer
+{
+    public class RepositoryFactory
+    {
+	    private readonly Dictionary<Type, IList<IEntityBase>> _objects = new Dictionary<Type, IList<IEntityBase>>(); 
+		public IRepository<T> CreateRepository<T>() where T : class, IEntityBase
+		{
+			IList<IEntityBase> itemsList;
+			if (!_objects.TryGetValue(typeof (T), out itemsList))
+			{
+				_objects[typeof(T)] = new List<IEntityBase>();
+			}
+
+			return new FakeRepository<T>((IList<T>) itemsList);
+		}
+		
+	    private static readonly object LockObject = new object();
+	    private static RepositoryFactory _instance;
+
+	    public static RepositoryFactory Instance
+	    {
+		    get
+		    {
+			    if (_instance == null)
+			    {
+					lock (LockObject)
+				    {
+					    if (_instance == null)
+					    {
+						    _instance = new RepositoryFactory();
+					    }
+				    }
+			    }
+			    return _instance;
+		    }
+	    }
+    }
+}
