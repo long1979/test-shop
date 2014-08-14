25c59bd172424423b0b3401f3c18eed7375b58a9
Viktor Borisov <vik.borisov@outlook.com>
2014-08-14 12:00:29 +0700

    потерянные файлы

---
+++ TestShop.DataLayer.Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace TestShop.DataLayer.Interfaces
+{
+	public interface IRepository<T> where T : IEntityBase
+	{
+		IQueryable<T> GetAll();
+
+		T Get(int entityId);
+		 
+		void Create(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+
+	}
+}
